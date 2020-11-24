8d86efa49eff509969508a86c55c5ce11ddbb874
sunsetmountain <brandon@sunsetmountain.com>
2020-11-23 18:21:53 -0700

    Create createVM.tf

---
+++ createVM.tf
@@ -0,0 +1,20 @@
+resource "google_compute_instance" "default" {
+  name         = "virtual-machine-from-terraform"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+}
